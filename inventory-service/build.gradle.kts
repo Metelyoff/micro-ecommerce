1776a5c175903cfa024681b1e7500c2ef701cac2
Denys Mietielov <metelyoff@gmail.com>
2025-02-09 01:19:46 +0100

    Fix actuator metrics exception: java.lang.NullPointerException: Cannot invoke "jdk.internal.platform.CgroupInfo.getMountPoint()" because "anyController" is null

--- inventory-service/build.gradle.kts
+++ inventory-service/build.gradle.kts
@@ -31,7 +31,7 @@ extra["springCloudVersion"] = "2024.0.0"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	implementation("com.github.Metelyoff:outbox:1.0.3")
