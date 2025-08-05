1277769a8800af966ef3fb0179981cf263d6afd5
Denys Mietielov <metelyoff@gmail.com>
2025-08-05 22:47:12 +0200

    Fix test

--- inventory-service/src/test/java/com/ecommerce/inventory/AbstractIntegrationTest.java
+++ inventory-service/src/test/java/com/ecommerce/inventory/AbstractIntegrationTest.java
@@ -1,14 +1,15 @@
 package com.ecommerce.inventory;
 
 import org.junit.jupiter.api.TestInstance;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.kafka.KafkaContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -22,15 +23,8 @@ public abstract class AbstractIntegrationTest {
             .withPassword("test")
             .withReuse(true);
 
-    private static final KafkaContainer KAFKA = new KafkaContainer(DockerImageName.parse("apache/kafka:latest"))
-//            .withEnv("KAFKA_KRAFT_MODE", "true")
-//            .withEnv("KAFKA_CFG_NODE_ID", "0")
-//            .withEnv("KAFKA_CFG_PROCESS_ROLES", "broker,controller")
-//            .withEnv("KAFKA_CFG_CONTROLLER_QUORUM_VOTERS", "0@localhost:9093")
-//            .withEnv("KAFKA_CFG_LISTENERS", "PLAINTEXT://:9092,CONTROLLER://:9093")
-//            .withEnv("KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP", "CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT")
-//            .withEnv("KAFKA_CFG_CONTROLLER_LISTENER_NAMES", "CONTROLLER")
-//            .withEnv("ALLOW_PLAINTEXT_LISTENER", "yes")
+    private static final ConfluentKafkaContainer KAFKA = new ConfluentKafkaContainer("confluentinc/cp-kafka")
+            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("Kafka")))
             .withReuse(true);
 
     static {
