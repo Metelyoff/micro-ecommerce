d06208f7c57e1c142a8b02e3ee87d4b3d6dcc464
Denys Mietielov <metelyoff@gmail.com>
2025-08-06 10:36:41 +0200

    Fix test

--- inventory-service/src/test/java/com/ecommerce/inventory/AbstractIntegrationTest.java
+++ inventory-service/src/test/java/com/ecommerce/inventory/AbstractIntegrationTest.java
@@ -2,15 +2,11 @@ package com.ecommerce.inventory;
 
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.TestInstance;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -24,22 +20,17 @@ public abstract class AbstractIntegrationTest {
     private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:15.4")
             .withDatabaseName("test-db")
             .withUsername("test")
-            .withPassword("test");
+            .withPassword("test")
+            .withReuse(true);
 
-    private static final ConfluentKafkaContainer KAFKA = new ConfluentKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka")
-            .asCompatibleSubstituteFor("confluentinc/cp-kafka"))
-            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(AbstractIntegrationTest.class)))
-            .waitingFor(Wait.forLogMessage(".*Kafka startTimeMs.*", 1));
+    private static final ConfluentKafkaContainer KAFKA =
+            new ConfluentKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka")
+                    .asCompatibleSubstituteFor("confluentinc/cp-kafka"))
+                    .withReuse(true);
 
     static {
         POSTGRES.start();
-        try {
-            KAFKA.start();
-        } catch (Exception e) {
-            System.err.println("Kafka failed to start:");
-            System.err.println(KAFKA.getLogs());
-            throw e;
-        }
+        KAFKA.start();
     }
 
     @DynamicPropertySource
