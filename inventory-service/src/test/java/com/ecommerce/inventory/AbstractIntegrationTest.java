168716eec8198c29ade741ffa6a5691c16574295
Denys Mietielov <metelyoff@gmail.com>
2025-10-24 16:33:56 +0200

    Fix inventory service tests

--- inventory-service/src/test/java/com/ecommerce/inventory/AbstractIntegrationTest.java
+++ inventory-service/src/test/java/com/ecommerce/inventory/AbstractIntegrationTest.java
@@ -27,29 +27,27 @@ public abstract class AbstractIntegrationTest {
             .withDatabaseName("test-db")
             .withUsername("test")
             .withPassword("test")
-            .withStartupTimeout(Duration.ofSeconds(120))
             .withReuse(true);
 
-//    private static final ConfluentKafkaContainer KAFKA =
-//            new ConfluentKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka")
-//                    .asCompatibleSubstituteFor("confluentinc/cp-kafka"))
-//                    .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("Kafka")))
-//                    .waitingFor(Wait.forLogMessage(".*Kafka startTimeMs.*", 1))
-//                    .withReuse(true);
+    private static final ConfluentKafkaContainer KAFKA =
+            new ConfluentKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka")
+                    .asCompatibleSubstituteFor("confluentinc/cp-kafka"))
+                    .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("Kafka")))
+                    .waitingFor(Wait.forLogMessage(".*Kafka startTimeMs.*", 1))
+                    .withReuse(true);
 
-    private static final KafkaContainer KAFKA = new KafkaContainer(DockerImageName.parse("apache/kafka:latest"))
-            .withEnv("KAFKA_KRAFT_MODE", "true")
-            .withEnv("KAFKA_CFG_NODE_ID", "0")
-            .withEnv("KAFKA_CFG_PROCESS_ROLES", "broker,controller")
-            .withEnv("KAFKA_CFG_CONTROLLER_QUORUM_VOTERS", "0@localhost:9093")
-            .withEnv("KAFKA_CFG_LISTENERS", "PLAINTEXT://:9092,CONTROLLER://:9093")
-            .withEnv("KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP", "CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT")
-            .withEnv("KAFKA_CFG_CONTROLLER_LISTENER_NAMES", "CONTROLLER")
-            .withEnv("ALLOW_PLAINTEXT_LISTENER", "yes")
-            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("Kafka")))
-            .waitingFor(Wait.forLogMessage(".*Kafka startTimeMs.*", 1))
-            .withStartupTimeout(Duration.ofSeconds(120))
-            .withReuse(true);
+//    private static final KafkaContainer KAFKA = new KafkaContainer(DockerImageName.parse("apache/kafka:latest"))
+//            .withEnv("KAFKA_KRAFT_MODE", "true")
+//            .withEnv("KAFKA_CFG_NODE_ID", "0")
+//            .withEnv("KAFKA_CFG_PROCESS_ROLES", "broker,controller")
+//            .withEnv("KAFKA_CFG_CONTROLLER_QUORUM_VOTERS", "0@localhost:9093")
+//            .withEnv("KAFKA_CFG_LISTENERS", "PLAINTEXT://:9092,CONTROLLER://:9093")
+//            .withEnv("KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP", "CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT")
+//            .withEnv("KAFKA_CFG_CONTROLLER_LISTENER_NAMES", "CONTROLLER")
+//            .withEnv("ALLOW_PLAINTEXT_LISTENER", "yes")
+//            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("Kafka")))
+//            .waitingFor(Wait.forLogMessage(".*Kafka startTimeMs.*", 1))
+//            .withReuse(true);
 
     static {
         POSTGRES.start();
