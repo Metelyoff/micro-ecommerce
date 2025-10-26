8f35399810e96903080df322d725ee428bf7b6b3
Denys Mietielov <metelyoff@gmail.com>
2025-10-26 18:22:03 +0100

    Fix FE envs

--- webapp/startup.sh
+++ webapp/startup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "$API_URL"
+echo "API_URL=${API_URL}"
 echo ""
 for file in /usr/share/nginx/html/static/js/*.js;
 do
-  envsubst "$API_URL" < "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+  envsubst "${API_URL}" < "$file" > "$file.tmp" && mv "$file.tmp" "$file"
 done
 
 BACKEND_ENVS=$(env | grep '^BACKEND_' | awk -F= '{print "\\$"$1}' | xargs)
-echo "$BACKEND_ENVS"
+echo "BACKEND_ENVS=$BACKEND_ENVS"
 echo ""
 envsubst "$BACKEND_ENVS" < nginx.template.conf > /etc/nginx/conf.d/default.conf
 
